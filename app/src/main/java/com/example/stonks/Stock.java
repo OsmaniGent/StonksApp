ecf5c6f05611a31eba0deea07768f3ee4a584c01
OsmaniGent <gosmani@york.citycollege.eu>
2024-07-10 01:04:32 +0200

    final

--- app/src/main/java/com/example/stonks/Stock.java
+++ app/src/main/java/com/example/stonks/Stock.java
@@ -1,20 +1,31 @@
 package com.example.stonks;
 
-public class Stock {
+import java.io.Serializable;
+
+public class Stock implements Serializable {
+
     private String name;
+    public String image;
     private double currentPrice;
     private double priceChange;
     private double priceChangePercentage;
+    private boolean isFavorite; // Added favorite flag
 
-    public Stock(String name, double currentPrice, double priceChange, double priceChangePercentage) {
+    public Stock(String name, String image, double currentPrice, double priceChange, double priceChangePercentage, boolean isFavorite) {
         this.name = name;
+        this.image = image;
         this.currentPrice = currentPrice;
         this.priceChange = priceChange;
         this.priceChangePercentage = priceChangePercentage;
+        this.isFavorite = isFavorite;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getImage() {
+        return image;
     }
 
     public double getCurrentPrice() {
@@ -27,5 +38,13 @@ public class Stock {
 
     public double getPriceChangePercentage() {
         return priceChangePercentage;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
     }
 }
